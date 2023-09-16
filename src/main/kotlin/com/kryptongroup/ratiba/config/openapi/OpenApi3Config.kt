914d85e6cb154fdc7c7e0b2dd07e122237873661
Ian kangethe <iankangethe@Ians-MacBook-Pro.local>
2023-09-16 13:35:59 +0300

    uri issue

--- src/main/kotlin/com/kryptongroup/ratiba/config/openapi/OpenApi3Config.kt
+++ src/main/kotlin/com/kryptongroup/ratiba/config/openapi/OpenApi3Config.kt
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.*
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.service.AuthorizationScope
