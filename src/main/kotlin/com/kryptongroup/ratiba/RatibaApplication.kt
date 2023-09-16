c19f2e10ab6433a2fbb4517ad687eed6cbc88d7e
Ian kangethe <iankangethe@Ians-MacBook-Pro.local>
2023-09-16 08:59:36 +0300

    starting out

---
+++ src/main/kotlin/com/kryptongroup/ratiba/RatibaApplication.kt
@@ -0,0 +1,11 @@
+package com.kryptongroup.ratiba
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RatibaApplication
+
+fun main(args: Array<String>) {
+	runApplication<RatibaApplication>(*args)
+}
---
+++ src/test/kotlin/com/kryptongroup/ratiba/RatibaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kryptongroup.ratiba
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RatibaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
